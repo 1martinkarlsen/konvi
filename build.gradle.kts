d103eb436748362f4a06f6a7da171a8ab5f64105
Martin Karlsen <1martinkarlsen@gmail.com>
2026-05-29 15:27:02 +0200

    Added detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,19 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.detekt)
 }
 
 allprojects {
     group = "com.konvi"
     version = "0.1.0-SNAPSHOT"
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    source.setFrom(
+        fileTree(rootDir) {
+            include("**/src/main/kotlin/**/*.kt")
+        }
+    )
 }
