ac5b2a8d1ec39915233e4f12bfd20080d60dfff2
Martin Karlsen <1martinkarlsen@gmail.com>
2026-06-09 11:23:02 +0200

    Added publish

--- ksp/build.gradle.kts
+++ ksp/build.gradle.kts
@@ -1,9 +1,19 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    `java-library`
+    `maven-publish`
 }
 
 kotlin {
     jvmToolchain(libs.versions.jvm.get().toInt())
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])   // artifactId defaults to the module name, e.g. "ksp"
+        }
+    }
 }
 
 dependencies {
