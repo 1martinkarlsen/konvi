4c11c185bc8a6098832c1ee9f18b318e5e82ef4c
Martin Karlsen <1martinkarlsen@gmail.com>
2026-06-13 00:31:50 +0200

    Added cli for new project

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 application {
     mainClass = "com.konvi.cli.MainKt"
+    applicationName = "konvi"
 }
 
 kotlin {
@@ -13,4 +14,27 @@ kotlin {
 
 dependencies {
     implementation(libs.clikt)
+}
+
+val templatesDir = layout.projectDirectory.dir("src/main/resources/templates")
+val templateIndexDir = layout.buildDirectory.dir("generated/templateIndex")
+
+val generateTemplateIndex by tasks.registering {
+    val root = templatesDir.asFile
+    val indexFile = templateIndexDir.get().dir("templates").asFile.resolve("index.txt")
+    inputs.dir(templatesDir)
+    outputs.dir(templateIndexDir)
+    doLast {
+        val entries = root.walkTopDown()
+            .filter { it.isFile }
+            .map { it.relativeTo(root).invariantSeparatorsPath }
+            .sorted()
+            .toList()
+        indexFile.parentFile.mkdirs()
+        indexFile.writeText(entries.joinToString("\n"))
+    }
+}
+
+sourceSets.main {
+    resources.srcDir(generateTemplateIndex)
 }
--- cli/src/main/kotlin/com/konvi/cli/Main.kt
+++ cli/src/main/kotlin/com/konvi/cli/Main.kt
@@ -8,9 +8,3 @@ fun main(args: Array<String>) = Konvi().subcommands(NewCommand()).main(args)
 class Konvi : CliktCommand(name = "konvi") {
     override fun run() = Unit
 }
-
-class NewCommand : CliktCommand(name = "new", help = "Scaffold a new Konvi project") {
-    override fun run() {
-        echo("Scaffolding new Konvi project...")
-    }
-}
