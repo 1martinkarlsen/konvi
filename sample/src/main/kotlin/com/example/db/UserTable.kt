dc33f87fff589597059eb0d5a8c808169e6de8d5
Martin Karlsen <1martinkarlsen@gmail.com>
2026-07-10 14:03:56 +0200

    Added name parameters

--- sample/src/main/kotlin/com/example/db/UserTable.kt
+++ sample/src/main/kotlin/com/example/db/UserTable.kt
@@ -4,9 +4,9 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 
 object UserTable : LongIdTable("user") {
-    val name = varchar(name = "name", 80)
-    val email = varchar(name = "email", 80)
-    val password = varchar(name = "password", 80)
+    val name = varchar(name = "name", length = 80)
+    val email = varchar(name = "email", length = 80)
+    val password = varchar(name = "password", length = 80)
     val createdAt = datetime(name = "created_at")
     val emailVerified = bool(name = "email_verified")
 }
